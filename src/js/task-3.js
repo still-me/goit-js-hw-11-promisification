015308b0b3fb6739ad44462b2ae06c09e7dd9f42
Vladyslav Fomenko <71770160+still-me@users.noreply.github.com>
2021-03-31 23:33:09 +0300

    Update task-3
    -change resolve argument to 1 object
    -change logSuccess params to destructive object from resolve

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -10,7 +10,7 @@ const makeTransaction = (transaction) => {
     const canProcess = Math.random() > 0.3;
 
     if (canProcess) {
-        resolve(transaction.id, delay);
+      resolve(transaction);
     }
         reject (transaction.id);
 
@@ -19,8 +19,8 @@ const makeTransaction = (transaction) => {
 
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, amount: time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
